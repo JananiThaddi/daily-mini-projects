ca8a10360ed115ddd63e40de325ae4ad61a67fae
Janani Mahalakshmi Prema Thaddi <jananithaddi@gmail.com>
2025-08-29 21:59:05 +0530

    Update password_generator.py

--- password_generator.py
+++ password_generator.py
@@ -6,7 +6,6 @@ def generate_password(length=12):
     characters = string.ascii_letters + string.digits + string.punctuation
     password = ''.join(random.choice(characters) for _ in range(length))
     return password
-
 # Ask user for desired password length
 try:
     length = int(input("Enter the desired password length (default 12): ") or 12)
